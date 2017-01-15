d16919883ad70d11bc2d6b693d870f0aa7db521c
Helms <helms+github@tekforze.com>
2017-01-14 20:57:24 -0500

    Create Widget Areas.php
    Simplified code to register and display WordPress sidebars / widget areas in your theme.

---
+++ Widget Areas.php
@@ -0,0 +1,23 @@
+/* ------------------------------------------------------
+Simplified code to registering and display widget 
+areas 1-5 in a theme
+--------------------------------------------------------- */
+
+// Register front-page widget areas 1-5
+for ( $i = 1; $i <= 5; $i++ ) {
+    genesis_register_widget_area(
+        array(
+		'id'	=> "front_page_widget_{$i}",
+		'name'	=> __( "Front Page Widget {$i}", 'GenStarter' ),
+		'description' => __( "This is the front page widget {$i} section.", 'GenStarter' ),
+        )
+    );
+}
+
+// Display Front Page Widget Areas 1-5
+for ( $i = 1; $i <= 5; $i++ ) {
+    genesis_widget_area( "front_page_widget_{$i}", array(
+        'before' => '<div class="front_page_widget widget_'.$i.'"><div class="widget-area"><div class="wrap">',
+        'after'  => '</div></div></div>',
+    ) );
+}
